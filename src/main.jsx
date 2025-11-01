bd77a4287c94cdb0300901d670feec4a341f8406
shanjid5566 <shanjidahmed66@gmail.com>
2025-11-01 21:17:23 +0600

    set rtk

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import "./index.css";
 import App from "./App.jsx";
 import { Route, Routes } from "react-router";
 import { BrowserRouter } from "react-router";
+import { Provider } from "react-redux";
+import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>
+  </Provider>
 );
